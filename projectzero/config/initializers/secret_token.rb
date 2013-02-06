8872ab6ae21d1f4458b10082666d8d80172cedcc
xlr8 <xlr8@mit.edu>
2013-02-06 07:46:26 -0800

    created second

--- projectzero/config/initializers/secret_token.rb
+++ projectzero/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Projectzero::Application.config.secret_token = 'e6554185e6042e029eed470de3136e587d11f9d12c1a7bb2a5bd65f17b006fbadcc8fc07be17755b31ce3a43016ef0aa00d675961abfacb09b35f4626322bcfc'
+Projectzero::Application.config.secret_token = '851fda4f32bb73d5412e037cf8067cf7582f14d7c7845fc0d2d9bc97c29d759a62fe7e696f707efd1c190c7c4c3403cd30b4f2714c30f22fe6cfebd9e20d3eda'
